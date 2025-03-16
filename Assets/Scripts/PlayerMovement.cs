3fd902eb07d8f94e7a86de407092502774ed3b37
cw1169 <cw1169@exeter.ac.uk>
2025-03-16 14:38:02 +0000

    Double Jump Implimentation

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -5,6 +5,7 @@ public class PlayerMovement : MonoBehaviour
 
     public float speed;
     public float jumpSpeed;
+    public float doubleJumpSpeed;
     public Transform groundCheck;
     public float groundCheckRadius;
     public LayerMask groundLayer;
@@ -12,8 +13,11 @@ public class PlayerMovement : MonoBehaviour
     private Rigidbody2D body;
     private float move;
     private bool isGrounded;
+    private int doubleJumpCount;
+    private bool isDoubleJump;
     private Animator playerAnimation;
     private SpriteRenderer spriteRenderer;
+    
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
@@ -23,7 +27,7 @@ public class PlayerMovement : MonoBehaviour
         spriteRenderer = GetComponent<SpriteRenderer>();
     }
 
-    // Update is called once per frame
+    // Update is called once per frame.
     void Update()
     {
         move = Input.GetAxis("Horizontal");
@@ -32,12 +36,23 @@ public class PlayerMovement : MonoBehaviour
         spriteRenderer.flipX = body.linearVelocity.x < 0;
 
         isGrounded = Physics2D.OverlapCircle(groundCheck.position, groundCheckRadius, groundLayer);
+        if(isGrounded){
+            doubleJumpCount = 1;
+            isDoubleJump = false;
+        }
 
         if(Input.GetButtonDown("Jump") && isGrounded){
             body.AddForce(new Vector2(body.linearVelocity.x, jumpSpeed));
+            isDoubleJump = false;
+        }
+        else if(Input.GetButtonDown("Jump") && doubleJumpCount > 0 && isGrounded == false){
+            body.linearVelocity = new Vector2(body.linearVelocity.x, doubleJumpSpeed);
+            doubleJumpCount = doubleJumpCount - 1;
+            isDoubleJump = true;
         }
 
         playerAnimation.SetFloat("Speed", Mathf.Abs(body.linearVelocity.x));
         playerAnimation.SetBool("isGrounded", isGrounded);
+        playerAnimation.SetBool("isDoubleJump", isDoubleJump);
     }
 }
