d3ac84ee42f4052bc46c54227bff1f4ec429f571
cw1169 <cw1169@exeter.ac.uk>
2025-03-17 06:31:37 +0000

    Added Death Animation and hurt Colour Flash

--- Assets/Scripts/PlayerHealth.cs
+++ Assets/Scripts/PlayerHealth.cs
@@ -1,3 +1,4 @@
+using System.Collections;
 using UnityEngine;
 
 public class PlayerHealth : MonoBehaviour
@@ -5,23 +6,43 @@ public class PlayerHealth : MonoBehaviour
     public int maxHealth = 100;
     public int health;
 
+    public bool isDead;
+
+    private Animator playerAnimation;
+    private Rigidbody2D body;
+    private SpriteRenderer spriteRenderer;
+
+
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
+        playerAnimation = GetComponent<Animator>();
+        body = GetComponent<Rigidbody2D>();
+        spriteRenderer = GetComponent<SpriteRenderer>();
         health = maxHealth;
+        isDead = false;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        playerAnimation.SetBool("isDead", isDead);
     }
 
     public void TakeDamage(int damage){
         health -= damage;
+        StartCoroutine(DamageFlash());
         if(health <= 0){
-            Destroy(gameObject);
+            isDead = true;
+            body.constraints = RigidbodyConstraints2D.FreezePositionX | RigidbodyConstraints2D.FreezePositionY;
         }
+    }
+
+    public IEnumerator DamageFlash(){
+        spriteRenderer.color = Color.magenta;
+        yield return new WaitForSeconds(0.2f);
+        spriteRenderer.color = Color.white;
+
     }
 }
